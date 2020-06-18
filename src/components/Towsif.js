7239cd6860dcf98e3a0937a253a4eeaf13f4b363
Towsif-Patwary <towsifict@gmail.com>
2020-06-19 01:09:23 +0600

    commit - {19-JUN-20} by Towsif

---
+++ src/components/Towsif.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Towsif extends Component {
+    render() {
+        return (
+            <div className="container">
+            <div className="row">
+                <div className="col-10 mx-auto text-center text-uppercase pt-5">
+                    <h6>Developed by-</h6>
+                    <h3 className="text-blue text-title">Mohammad Towsif Patwary</h3>
+                    <h4>Software Developer</h4>
+                    <p>Mobile: +8801670115405<br/>Email: mdtowsif03@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        );
+    }
+}
